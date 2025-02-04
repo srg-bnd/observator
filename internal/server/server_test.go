c37c776f88bebcb0b1fe7fe1029982469ceb1c95
Sergey Bondarenko <sb.truefalse@gmail.com>
2025-02-04 14:56:12 +0300

    Tests for server

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -1,1 +1,18 @@
 package server
+
+import (
+	"testing"
+)
+
+func TestStart(t *testing.T) {
+	// TODO
+}
+
+func TestGetHost(t *testing.T) {
+	got, _ := getHost()
+	want := ":8080"
+
+	if got != want {
+		t.Errorf(`Incorrect 'getHost()' method behavior, got "%v", want "%v"`, got, want)
+	}
+}
