0510e587a3d76cd818bd5c6ae521695c43993f68
Sergey Bondarenko <sb.truefalse@gmail.com>
2025-02-12 19:20:06 +0300

    Refactoring

--- internal/agent/collector/collector.go
+++ internal/agent/collector/collector.go
@@ -3,53 +3,83 @@ package collector
 type Collector struct {
 }
 
+type CounterMetric struct {
+	Name  string
+	Value int64
+}
+
+type GaugeMetric struct {
+	Name  string
+	Value float64
+}
+
+func NewCounterMetric(name string, value int64) *CounterMetric {
+	return &CounterMetric{
+		Name:  name,
+		Value: value,
+	}
+}
+
+func NewGaugeMetric(name string, value float64) *GaugeMetric {
+	return &GaugeMetric{
+		Name:  name,
+		Value: value,
+	}
+}
+
+type Metrics struct {
+	Counter *[]CounterMetric
+	Gauge   *[]GaugeMetric
+}
+
+func NewMetrics(counter *[]CounterMetric, gauge *[]GaugeMetric) *Metrics {
+	return &Metrics{
+		Counter: counter,
+		Gauge:   gauge,
+	}
+}
+
 func NewCollector() *Collector {
 	return &Collector{}
 }
 
-func (c *Collector) GetMetrics() (map[string][]string, error) {
-	metrics := make(map[string][]string, 0)
-
-	metrics["counter"] = []string{
-		"PollCount",
-	}
-	metrics["gauge"] = []string{
-		"Alloc",
-		"BuckHashSys",
-		"Frees",
-		"GCCPUFraction",
-		"GCSys",
-
-		"HeapAlloc",
-		"HeapIdle",
-		"HeapInuse",
-		"HeapObjects",
-		"HeapReleased",
-		"HeapSys",
-
-		"LastGC",
-		"Lookups",
-		"MCacheInuse",
-		"MCacheSys",
-		"MSpanInuse",
-		"MSpanSys",
-
-		"Mallocs",
-		"NextGC",
-		"NumForcedGC",
-		"NumGC",
-		"NumForcedGC",
-		"NumGC",
-
-		"OtherSys",
-		"PauseTotalNs",
-		"StackInuse",
-		"StackSys",
-		"Sys",
-		"TotalAlloc",
-
-		"RandomValue",
-	}
-
-	return metrics, nil
+func (c *Collector) GetMetrics() (*Metrics, error) {
+	return NewMetrics(
+		&[]CounterMetric{
+			// Custom
+			*NewCounterMetric("PollCount", 0),
+		},
+		&[]GaugeMetric{
+			// Runtime
+			*NewGaugeMetric("Alloc", 0.0),
+			*NewGaugeMetric("BuckHashSys", 0.0),
+			*NewGaugeMetric("Frees", 0.0),
+			*NewGaugeMetric("GCCPUFraction", 0.0),
+			*NewGaugeMetric("GCSys", 0.0),
+			*NewGaugeMetric("HeapAlloc", 0.0),
+			*NewGaugeMetric("HeapIdle", 0.0),
+			*NewGaugeMetric("HeapInuse", 0.0),
+			*NewGaugeMetric("HeapObjects", 0.0),
+			*NewGaugeMetric("HeapReleased", 0.0),
+			*NewGaugeMetric("HeapSys", 0.0),
+			*NewGaugeMetric("LastGC", 0.0),
+			*NewGaugeMetric("Lookups", 0.0),
+			*NewGaugeMetric("MCacheInuse", 0.0),
+			*NewGaugeMetric("MCacheSys", 0.0),
+			*NewGaugeMetric("MSpanInuse", 0.0),
+			*NewGaugeMetric("MSpanSys", 0.0),
+			*NewGaugeMetric("Mallocs", 0.0),
+			*NewGaugeMetric("NextGC", 0.0),
+			*NewGaugeMetric("NumForcedGC", 0.0),
+			*NewGaugeMetric("NumGC", 0.0),
+			*NewGaugeMetric("OtherSys", 0.0),
+			*NewGaugeMetric("PauseTotalNs", 0.0),
+			*NewGaugeMetric("StackInuse", 0.0),
+			*NewGaugeMetric("StackSys", 0.0),
+			*NewGaugeMetric("Sys", 0.0),
+			*NewGaugeMetric("TotalAlloc", 0.0),
+			// Custom
+			*NewGaugeMetric("RandomValue", 0.0),
+		},
+	), nil
 }
--- internal/agent/poller/poller.go
+++ internal/agent/poller/poller.go
@@ -30,25 +30,26 @@ func (r *Poller) Start() {
 	for {
 		time.Sleep(GetPollInterval())
 		log.Println("=== Poller started ===")
-		metricsByType, err := r.collector.GetMetrics()
+		metrics, err := r.collector.GetMetrics()
 
 		if err != nil {
 			log.Println(err)
 			return
 		}
-		ShowMetrics(metricsByType)
+		ShowMetrics(metrics)
 		log.Println("=== Poller stopped ===")
 	}
 }
 
-func ShowMetrics(metricsByType map[string][]string) {
-	fmt.Println("Gauge:")
+func ShowMetrics(metrics *collector.Metrics) {
+	fmt.Println("Counter:")
+	for _, counterMetric := range *metrics.Counter {
+		fmt.Println("-", counterMetric.Name, ":", counterMetric.Value)
+	}
 
-	for typeOfMetric, metrics := range metricsByType {
-		fmt.Println(typeOfMetric, ":")
-		for _, metric := range metrics {
-			fmt.Println("-", metric, ":", 0)
-		}
+	fmt.Println("Gauge:")
+	for _, gaugeMetric := range *metrics.Gauge {
+		fmt.Println("-", gaugeMetric.Name, ":", gaugeMetric.Value)
 	}
 }
 
