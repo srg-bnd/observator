729c1914624fcc7e0bd1120243282552161e13ad
Sergey Bondarenko <sb.truefalse@gmail.com>
2025-02-12 13:57:04 +0300

    Add tests

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -2,7 +2,16 @@ package handlers
 
 import (
 	"testing"
+
+	"github.com/srg-bnd/observator/internal/storage"
+	"github.com/stretchr/testify/assert"
 )
+
+func TestNewHttpHandler(t *testing.T) {
+	storage := storage.NewMemStorage()
+	deque := NewHttpHandler(storage)
+	assert.IsType(t, deque, &HttpHandler{})
+}
 
 func TestUpdateMetricHandler(t *testing.T) {
 	t.Logf("TODO")
--- internal/services/services_test.go
+++ internal/services/services_test.go
@@ -2,7 +2,16 @@ package services
 
 import (
 	"testing"
+
+	"github.com/srg-bnd/observator/internal/storage"
+	"github.com/stretchr/testify/assert"
 )
+
+func TestNewService(t *testing.T) {
+	storage := storage.NewMemStorage()
+	deque := NewService(storage)
+	assert.IsType(t, deque, &Service{})
+}
 
 func TestUpdateMetricService(t *testing.T) {
 	// TODO
