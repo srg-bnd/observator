e45e7d778fe74f46056cddd2577edf452cb94239
Sergey Bondarenko <sb.truefalse@gmail.com>
2025-02-13 04:25:57 +0300

    Save metrics

--- internal/agent/collector/collector.go
+++ internal/agent/collector/collector.go
@@ -1,6 +1,10 @@
 package collector
 
-import "slices"
+import (
+	"math/rand"
+	"runtime"
+	"slices"
+)
 
 type Collector struct {
 	trackedMetrics *TrackedMetrics
@@ -186,119 +190,119 @@ func (c *Collector) GetMetrics() (*Metrics, error) {
 // Counter Metrics
 
 func pollCountCounterMetric() int64 {
-	return 0
+	return 1
 }
 
 // Gauge Metrics
 
 func allocGaugeMetric() float64 {
-	return 0.0
+	return float64(runtime.MemStats.Alloc)
 }
 
 func buckHashSysGaugeMetric() float64 {
-	return 0.0
+	return float64(runtime.MemStats.BuckHashSys)
 }
 
 func freesGaugeMetric() float64 {
-	return 0.0
+	return float64(runtime.MemStats.Frees)
 }
 
 func gCCPUFractionGaugeMetric() float64 {
-	return 0.0
+	return float64(runtime.MemStats.GCCPUFraction)
 }
 
 func gCSysGaugeMetric() float64 {
-	return 0.0
+	return float64(runtime.MemStats.GCSys)
 }
 
 func heapAllocGaugeMetric() float64 {
-	return 0.0
+	return float64(runtime.MemStats.HeapAlloc)
 }
 
 func heapIdleGaugeMetric() float64 {
-	return 0.0
+	return float64(runtime.MemStats.HeapIdle)
 }
 
 func heapInuseGaugeMetric() float64 {
-	return 0.0
+	return float64(runtime.MemStats.HeapInuse)
 }
 
 func heapObjectsGaugeMetric() float64 {
-	return 0.0
+	return float64(runtime.MemStats.HeapObjects)
 }
 
 func heapReleasedGaugeMetric() float64 {
-	return 0.0
+	return float64(runtime.MemStats.HeapReleased)
 }
 
 func heapSysGaugeMetric() float64 {
-	return 0.0
+	return float64(runtime.MemStats.HeapSys)
 }
 
 func lastGCGaugeMetric() float64 {
-	return 0.0
+	return float64(runtime.MemStats.LastGC)
 }
 
 func lookupsGaugeMetric() float64 {
-	return 0.0
+	return float64(runtime.MemStats.Lookups)
 }
 
 func mCacheInuseGaugeMetric() float64 {
-	return 0.0
+	return float64(runtime.MemStats.MCacheInuse)
 }
 
 func mCacheSysGaugeMetric() float64 {
-	return 0.0
+	return float64(runtime.MemStats.MCacheSys)
 }
 
 func mSpanInuseGaugeMetric() float64 {
-	return 0.0
+	return float64(runtime.MemStats.MSpanInuse)
 }
 
 func mSpanSysGaugeMetric() float64 {
-	return 0.0
+	return float64(runtime.MemStats.MSpanSys)
 }
 
 func mallocsGaugeMetric() float64 {
-	return 0.0
+	return float64(runtime.MemStats.Mallocs)
 }
 
 func nextGCGaugeMetric() float64 {
-	return 0.0
+	return float64(runtime.MemStats.NextGC)
 }
 
 func numForcedGCGaugeMetric() float64 {
-	return 0.0
+	return float64(runtime.MemStats.NumForcedGC)
 }
 
 func numGCGaugeMetric() float64 {
-	return 0.0
+	return float64(runtime.MemStats.NumGC)
 }
 
 func otherSysGaugeMetric() float64 {
-	return 0.0
+	return float64(runtime.MemStats.OtherSys)
 }
 
 func pauseTotalNsGaugeMetric() float64 {
-	return 0.0
+	return float64(runtime.MemStats.PauseTotalNs)
 }
 
 func stackInuseGaugeMetric() float64 {
-	return 0.0
+	return float64(runtime.MemStats.StackInuse)
 }
 
 func stackSysGaugeMetric() float64 {
-	return 0.0
+	return float64(runtime.MemStats.StackSys)
 }
 
 func sysGaugeMetric() float64 {
-	return 0.0
+	return float64(runtime.MemStats.Sys)
 }
 
 func totalAllocGaugeMetric() float64 {
-	return 0.0
+	return float64(runtime.MemStats.TotalAlloc)
 }
 
 func randomValueGaugeMetric() float64 {
-	return 0.0
+	return rand.ExpFloat64()
 }
--- internal/agent/services/services.go
+++ internal/agent/services/services.go
@@ -1,8 +1,6 @@
 package services
 
 import (
-	"fmt"
-
 	"github.com/srg-bnd/observator/internal/agent/collector"
 	"github.com/srg-bnd/observator/internal/storage"
 )
@@ -18,19 +16,13 @@ func NewService(storage storage.Repositories) *Service {
 }
 
 func (s *Service) MetricsUpdateService(metrics *collector.Metrics) error {
-	ShowMetrics(metrics)
+	for _, metric := range metrics.Counter {
+		s.storage.SetCounter(metric.Name, metric.Value)
+	}
+
+	for _, metric := range metrics.Gauge {
+		s.storage.SetGauge(metric.Name, metric.Value)
+	}
 
 	return nil
-}
-
-func ShowMetrics(metrics *collector.Metrics) {
-	fmt.Println("Counter:")
-	for _, counterMetric := range metrics.Counter {
-		fmt.Println("-", counterMetric.Name, ":", counterMetric.Value)
-	}
-
-	fmt.Println("Gauge:")
-	for _, gaugeMetric := range metrics.Gauge {
-		fmt.Println("-", gaugeMetric.Name, ":", gaugeMetric.Value)
-	}
 }
