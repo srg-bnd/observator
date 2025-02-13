981f8e597919bc5b84c3dd9c8924e868ff7cb096
Sergey Bondarenko <sb.truefalse@gmail.com>
2025-02-13 05:22:18 +0300

    Fix tests

--- internal/agent/client/client_test.go
+++ internal/agent/client/client_test.go
@@ -3,11 +3,12 @@ package client
 import (
 	"testing"
 
+	"github.com/srg-bnd/observator/internal/storage"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNewClient(t *testing.T) {
-	client := NewClient()
+	client := NewClient(storage.NewMemStorage())
 	assert.IsType(t, client, &Client{})
 }
 
