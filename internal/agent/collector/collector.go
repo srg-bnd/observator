08eb7ab119306c620c0fdfed78a50015c72ff3e9
Sergey Bondarenko <sb.truefalse@gmail.com>
2025-02-13 04:03:23 +0300

    Fix collector

--- internal/agent/collector/collector.go
+++ internal/agent/collector/collector.go
@@ -73,120 +73,232 @@ func (c *Collector) GetMetrics() (*Metrics, error) {
 	}
 
 	if slices.Contains(c.trackedMetrics.Gauge, "BuckHashSys") {
-		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("BuckHashSys", allocGaugeMetric()))
+		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("BuckHashSys", buckHashSysGaugeMetric()))
 	}
 
 	if slices.Contains(c.trackedMetrics.Gauge, "Frees") {
-		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("Frees", allocGaugeMetric()))
+		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("Frees", freesGaugeMetric()))
 	}
 
 	if slices.Contains(c.trackedMetrics.Gauge, "GCCPUFraction") {
-		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("GCCPUFraction", allocGaugeMetric()))
+		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("GCCPUFraction", gCCPUFractionGaugeMetric()))
 	}
 
 	if slices.Contains(c.trackedMetrics.Gauge, "GCSys") {
-		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("GCSys", allocGaugeMetric()))
+		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("GCSys", gCSysGaugeMetric()))
 	}
 
 	if slices.Contains(c.trackedMetrics.Gauge, "HeapAlloc") {
-		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("HeapAlloc", allocGaugeMetric()))
+		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("HeapAlloc", heapAllocGaugeMetric()))
 	}
 
 	if slices.Contains(c.trackedMetrics.Gauge, "HeapIdle") {
-		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("HeapIdle", allocGaugeMetric()))
+		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("HeapIdle", heapIdleGaugeMetric()))
 	}
 
 	if slices.Contains(c.trackedMetrics.Gauge, "HeapInuse") {
-		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("HeapInuse", allocGaugeMetric()))
+		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("HeapInuse", heapInuseGaugeMetric()))
 	}
 
 	if slices.Contains(c.trackedMetrics.Gauge, "HeapObjects") {
-		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("HeapObjects", allocGaugeMetric()))
+		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("HeapObjects", heapObjectsGaugeMetric()))
 	}
 
 	if slices.Contains(c.trackedMetrics.Gauge, "HeapReleased") {
-		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("HeapReleased", allocGaugeMetric()))
+		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("HeapReleased", heapReleasedGaugeMetric()))
 	}
 
 	if slices.Contains(c.trackedMetrics.Gauge, "HeapSys") {
-		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("HeapSys", allocGaugeMetric()))
+		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("HeapSys", heapSysGaugeMetric()))
 	}
 
 	if slices.Contains(c.trackedMetrics.Gauge, "LastGC") {
-		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("LastGC", allocGaugeMetric()))
+		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("LastGC", lastGCGaugeMetric()))
 	}
 
 	if slices.Contains(c.trackedMetrics.Gauge, "Lookups") {
-		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("Lookups", allocGaugeMetric()))
+		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("Lookups", lookupsGaugeMetric()))
 	}
 
 	if slices.Contains(c.trackedMetrics.Gauge, "MCacheInuse") {
-		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("MCacheInuse", allocGaugeMetric()))
+		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("MCacheInuse", mCacheInuseGaugeMetric()))
 	}
 
 	if slices.Contains(c.trackedMetrics.Gauge, "MCacheSys") {
-		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("MCacheSys", allocGaugeMetric()))
+		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("MCacheSys", mCacheSysGaugeMetric()))
 	}
 
 	if slices.Contains(c.trackedMetrics.Gauge, "MSpanInuse") {
-		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("MSpanInuse", allocGaugeMetric()))
+		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("MSpanInuse", mSpanInuseGaugeMetric()))
 	}
 
 	if slices.Contains(c.trackedMetrics.Gauge, "MSpanSys") {
-		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("MSpanSys", allocGaugeMetric()))
+		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("MSpanSys", mSpanSysGaugeMetric()))
 	}
 
 	if slices.Contains(c.trackedMetrics.Gauge, "Mallocs") {
-		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("Mallocs", allocGaugeMetric()))
+		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("Mallocs", mallocsGaugeMetric()))
 	}
 
 	if slices.Contains(c.trackedMetrics.Gauge, "NextGC") {
-		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("NextGC", allocGaugeMetric()))
+		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("NextGC", nextGCGaugeMetric()))
 	}
 
 	if slices.Contains(c.trackedMetrics.Gauge, "NumForcedGC") {
-		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("NumForcedGC", allocGaugeMetric()))
+		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("NumForcedGC", numForcedGCGaugeMetric()))
 	}
 
 	if slices.Contains(c.trackedMetrics.Gauge, "NumGC") {
-		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("NumGC", allocGaugeMetric()))
+		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("NumGC", numGCGaugeMetric()))
 	}
 
 	if slices.Contains(c.trackedMetrics.Gauge, "OtherSys") {
-		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("OtherSys", allocGaugeMetric()))
+		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("OtherSys", otherSysGaugeMetric()))
 	}
 
 	if slices.Contains(c.trackedMetrics.Gauge, "PauseTotalNs") {
-		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("PauseTotalNs", allocGaugeMetric()))
+		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("PauseTotalNs", pauseTotalNsGaugeMetric()))
 	}
 
 	if slices.Contains(c.trackedMetrics.Gauge, "StackInuse") {
-		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("StackInuse", allocGaugeMetric()))
+		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("StackInuse", stackInuseGaugeMetric()))
 	}
 
 	if slices.Contains(c.trackedMetrics.Gauge, "StackSys") {
-		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("StackSys", allocGaugeMetric()))
+		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("StackSys", stackSysGaugeMetric()))
 	}
 
 	if slices.Contains(c.trackedMetrics.Gauge, "Sys") {
-		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("Sys", allocGaugeMetric()))
+		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("Sys", sysGaugeMetric()))
 	}
 
 	if slices.Contains(c.trackedMetrics.Gauge, "TotalAlloc") {
-		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("TotalAlloc", allocGaugeMetric()))
+		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("TotalAlloc", totalAllocGaugeMetric()))
 	}
 
 	if slices.Contains(c.trackedMetrics.Gauge, "RandomValue") {
-		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("RandomValue", allocGaugeMetric()))
+		gaugeMetrics = append(gaugeMetrics, *NewGaugeMetric("RandomValue", randomValueGaugeMetric()))
 	}
 
 	return NewMetrics(counterMetrics, gaugeMetrics), nil
 }
 
+// Counter Metrics
+
 func pollCountCounterMetric() int64 {
 	return 0
 }
 
+// Gauge Metrics
+
 func allocGaugeMetric() float64 {
+	return 0.0
+}
+
+func buckHashSysGaugeMetric() float64 {
+	return 0.0
+}
+
+func freesGaugeMetric() float64 {
+	return 0.0
+}
+
+func gCCPUFractionGaugeMetric() float64 {
+	return 0.0
+}
+
+func gCSysGaugeMetric() float64 {
+	return 0.0
+}
+
+func heapAllocGaugeMetric() float64 {
+	return 0.0
+}
+
+func heapIdleGaugeMetric() float64 {
+	return 0.0
+}
+
+func heapInuseGaugeMetric() float64 {
+	return 0.0
+}
+
+func heapObjectsGaugeMetric() float64 {
+	return 0.0
+}
+
+func heapReleasedGaugeMetric() float64 {
+	return 0.0
+}
+
+func heapSysGaugeMetric() float64 {
+	return 0.0
+}
+
+func lastGCGaugeMetric() float64 {
+	return 0.0
+}
+
+func lookupsGaugeMetric() float64 {
+	return 0.0
+}
+
+func mCacheInuseGaugeMetric() float64 {
+	return 0.0
+}
+
+func mCacheSysGaugeMetric() float64 {
+	return 0.0
+}
+
+func mSpanInuseGaugeMetric() float64 {
+	return 0.0
+}
+
+func mSpanSysGaugeMetric() float64 {
+	return 0.0
+}
+
+func mallocsGaugeMetric() float64 {
+	return 0.0
+}
+
+func nextGCGaugeMetric() float64 {
+	return 0.0
+}
+
+func numForcedGCGaugeMetric() float64 {
+	return 0.0
+}
+
+func numGCGaugeMetric() float64 {
+	return 0.0
+}
+
+func otherSysGaugeMetric() float64 {
+	return 0.0
+}
+
+func pauseTotalNsGaugeMetric() float64 {
+	return 0.0
+}
+
+func stackInuseGaugeMetric() float64 {
+	return 0.0
+}
+
+func stackSysGaugeMetric() float64 {
+	return 0.0
+}
+
+func sysGaugeMetric() float64 {
+	return 0.0
+}
+
+func totalAllocGaugeMetric() float64 {
+	return 0.0
+}
+
+func randomValueGaugeMetric() float64 {
 	return 0.0
 }
--- internal/agent/collector/collector_test.go
+++ internal/agent/collector/collector_test.go
@@ -18,23 +18,143 @@ func TestNewTrackedMetrics(t *testing.T) {
 }
 
 func TestNewCounterMetric(t *testing.T) {
-	counterMetric := NewCounterMetric("metric", 1.0)
+	counterMetric := NewCounterMetric("metric", 1)
 	assert.IsType(t, counterMetric, &CounterMetric{})
 }
 
 func TestNewGaugeMetric(t *testing.T) {
-	gaugeMetric := NewGaugeMetric("metric", 1.0)
+	gaugeMetric := NewGaugeMetric("metric", 1)
 	assert.IsType(t, gaugeMetric, &GaugeMetric{})
 }
 
 func TestNewMetrics(t *testing.T) {
 	metrics := NewMetrics(
 		[]CounterMetric{*NewCounterMetric("metric", 1)},
-		[]GaugeMetric{*NewGaugeMetric("metric", 1.0)},
+		[]GaugeMetric{*NewGaugeMetric("metric", 1)},
 	)
 	assert.IsType(t, metrics, &Metrics{})
 }
 
 func TestGetMetrics(t *testing.T) {
+	t.Logf("TODO")
+}
+
+// Counter Metrics
+
+func TestPollCountCounterMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+// Gauge Metrics
+
+func TestAllocGaugeMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+func TestBuckHashSysGaugeMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+func TestFreesGaugeMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+func TestGCCPUFractionGaugeMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+func TestGCSysGaugeMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+func TestHeapAllocGaugeMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+func TestheapIdleGaugeMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+func TestHeapInuseGaugeMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+func TestHeapObjectsGaugeMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+func TestHeapReleasedGaugeMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+func TestHeapSysGaugeMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+func TestLastGCGaugeMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+func TestLookupsGaugeMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+func TestMCacheInuseGaugeMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+func TestMCacheSysGaugeMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+func TestMSpanInuseGaugeMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+func TestMSpanSysGaugeMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+func TestMallocsGaugeMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+func TestNextGCGaugeMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+func TestNumForcedGCGaugeMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+func TestNumGCGaugeMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+func TestOtherSysGaugeMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+func TestPauseTotalNsGaugeMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+func TestStackInuseGaugeMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+func TestStackSysGaugeMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+func TestSysGaugeMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+func TestTotalAllocGaugeMetric(t *testing.T) {
+	t.Logf("TODO")
+}
+
+func TestRandomValueGaugeMetric(t *testing.T) {
 	t.Logf("TODO")
 }
