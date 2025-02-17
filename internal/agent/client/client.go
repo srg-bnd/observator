ba48e181059fb151ce1614aa6b98480dcdc4408f
Sergey Bondarenko <sb.truefalse@gmail.com>
2025-02-18 02:06:43 +0300

    Fix client

--- internal/agent/client/client.go
+++ internal/agent/client/client.go
@@ -24,18 +24,18 @@ func NewClient() *Client {
 
 func (c *Client) SendMetric(metricType string, metricName string, metricValue string) {
 	endpoint := getBaseURL() + "/update/" + metricType + "/" + metricName + "/" + string(metricValue) + "/"
-	request, err := http.NewRequest(http.MethodPost, endpoint, nil)
+	request, err := http.NewRequest(http.MethodPost, endpoint, http.NoBody)
 	if err != nil {
 		panic(err)
 	}
 
 	request.Header.Set("Content-Type", "text/plain; charset=UTF-8")
-	_, err = c.client.Do(request)
+	response, err := c.client.Do(request)
 	if err != nil {
 		log.Println(err)
+	} else {
+		defer response.Body.Close()
 	}
-
-	// defer response.Body.Close()
 }
 
 func getBaseURL() string {
