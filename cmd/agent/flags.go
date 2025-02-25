22331ae2971323f478740f2db659af66c24d38db
Sergey Bondarenko <45511623+srg-bnd@users.noreply.github.com>
2025-02-26 02:01:03 +0300

    (feat) Run-envs for server & agent (#5)
    * Add ADDRESS, REPORT_INTERVAL and POLL_INTERVAL envs for agent
    * Add ADDRESS env for server

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -1,7 +1,11 @@
 // Flags
 package main
 
-import "flag"
+import (
+	"flag"
+	"os"
+	"strconv"
+)
 
 var AgentFlags = struct {
 	pollInterval   int
@@ -10,8 +14,21 @@ var AgentFlags = struct {
 }{}
 
 func parseFlags() {
-	flag.StringVar(&AgentFlags.serverAddr, "a", "localhost:8080", "address and port to run server")
-	flag.IntVar(&AgentFlags.reportInterval, "r", 10, "reportInterval – frequency (seconds) of sending values to the server")
 	flag.IntVar(&AgentFlags.pollInterval, "p", 2, "pollInterval – frequency (seconds) of metric polling")
+	flag.IntVar(&AgentFlags.reportInterval, "r", 10, "reportInterval – frequency (seconds) of sending values to the server")
+	flag.StringVar(&AgentFlags.serverAddr, "a", "localhost:8080", "address and port to run server")
+
 	flag.Parse()
+
+	if envPollInterval := os.Getenv("POLL_INTERVAL"); envPollInterval != "" {
+		AgentFlags.pollInterval, _ = strconv.Atoi(envPollInterval)
+	}
+
+	if envReportInterval := os.Getenv("REPORT_INTERVAL"); envReportInterval != "" {
+		AgentFlags.reportInterval, _ = strconv.Atoi(envReportInterval)
+	}
+
+	if envServerAddr := os.Getenv("ADDRESS"); envServerAddr != "" {
+		AgentFlags.serverAddr = envServerAddr
+	}
 }
--- cmd/server/flags.go
+++ cmd/server/flags.go
@@ -1,11 +1,18 @@
 // Flags
 package main
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 var flagHostAddr string
 
 func parseFlags() {
 	flag.StringVar(&flagHostAddr, "a", ":8080", "address and port to run server")
 	flag.Parse()
+
+	if envHostAddr := os.Getenv("ADDRESS"); envHostAddr != "" {
+		flagHostAddr = envHostAddr
+	}
 }
