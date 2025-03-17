7a544f31eb559662f0b5f48ff28bd73618710af8
Sergey Bondarenko <sb.truefalse@gmail.com>
2025-03-17 22:09:47 +0300

    feat: New flags

--- cmd/server/flags.go
+++ cmd/server/flags.go
@@ -4,11 +4,15 @@ package main
 import (
 	"flag"
 	"os"
+	"strconv"
 )
 
 type AppConfigs struct {
-	flagHostAddr string
-	flagLogLevel string
+	flagHostAddr        string
+	flagLogLevel        string
+	flagStoreInterval   int
+	flagFileStoragePath string
+	flagRestore         bool
 }
 
 var appConfigs = AppConfigs{}
@@ -16,6 +20,10 @@ var appConfigs = AppConfigs{}
 func parseFlags() {
 	flag.StringVar(&appConfigs.flagHostAddr, "a", ":8080", "address and port to run server")
 	flag.StringVar(&appConfigs.flagLogLevel, "l", "info", "log level")
+	// Files
+	flag.IntVar(&appConfigs.flagStoreInterval, "i", 300, "store interval in seconds (zero for sync)")
+	flag.StringVar(&appConfigs.flagFileStoragePath, "f", "~/temp.storage", "file storage path")
+	flag.BoolVar(&appConfigs.flagRestore, "r", true, "load data from storage")
 	flag.Parse()
 
 	if envHostAddr := os.Getenv("ADDRESS"); envHostAddr != "" {
@@ -23,5 +31,17 @@ func parseFlags() {
 	}
 	if envLogLevel := os.Getenv("LOG_LEVEL"); envLogLevel != "" {
 		appConfigs.flagLogLevel = envLogLevel
+	}
+	// Files
+	if envStoreInterval := os.Getenv("STORE_INTERVAL"); envStoreInterval != "" {
+		value, _ := strconv.ParseInt(envStoreInterval, 10, 0)
+		appConfigs.flagStoreInterval = int(value)
+	}
+	if envFileStoragePath := os.Getenv("FILE_STORAGE_PATH"); envFileStoragePath != "" {
+		appConfigs.flagFileStoragePath = envFileStoragePath
+	}
+	if envRestore := os.Getenv("RESTORE"); envRestore != "" {
+		value, _ := strconv.ParseBool(envRestore)
+		appConfigs.flagRestore = value
 	}
 }
