c5bebecf80064ac9e584e42b92d6b045aa37421a
Sergey Bondarenko <sb.truefalse@gmail.com>
2025-05-30 15:40:04 +0300

    fix: add rw-mutex for mem-storage

--- internal/storage/mem_storage.go
+++ internal/storage/mem_storage.go
@@ -3,12 +3,14 @@ package storage
 
 import (
 	"errors"
+	"sync"
 )
 
 // Mem storage
 type MemStorage struct {
 	gauges   map[string]gauge
 	counters map[string]counter
+	mtx      sync.RWMutex
 }
 
 // Returns a new mem storage
@@ -16,39 +18,50 @@ func NewMemStorage() *MemStorage {
 	return &MemStorage{
 		gauges:   make(map[string]gauge),
 		counters: make(map[string]counter),
+		mtx:      sync.RWMutex{},
 	}
 }
 
 // Changes gauge by key
 func (mStore *MemStorage) SetGauge(key string, value float64) error {
+	mStore.mtx.Lock()
 	mStore.gauges[key] = gauge(value)
+	mStore.mtx.Unlock()
 
 	return nil
 }
 
 // Returns gauge by key
 func (mStore *MemStorage) GetGauge(key string) (float64, error) {
+	mStore.mtx.RLock()
+
 	value, ok := mStore.gauges[key]
 	if !ok {
 		return -1, errors.New("unknown")
 	}
+
+	mStore.mtx.RUnlock()
 
 	return float64(value), nil
 }
 
 // Changes counter by key
 func (mStore *MemStorage) SetCounter(key string, value int64) error {
+	mStore.mtx.Lock()
 	mStore.counters[key] += counter(value)
+	mStore.mtx.Unlock()
 
 	return nil
 }
 
 // Returns gauge by counter
 func (mStore *MemStorage) GetCounter(key string) (int64, error) {
+	mStore.mtx.RLock()
 	value, ok := mStore.counters[key]
 	if !ok {
 		return -1, errors.New("unknown")
 	}
+	mStore.mtx.RUnlock()
 
 	return int64(value), nil
 }
