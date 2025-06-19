3c4c5d73f99f218620818164984ca9acdd04daaa
Sergey Bondarenko <sb.truefalse@gmail.com>
2025-06-20 00:31:55 +0300

    chore(server): set checksum for body

--- internal/server/middleware/checksum.go
+++ internal/server/middleware/checksum.go
@@ -2,18 +2,27 @@
 package middleware
 
 import (
+	"bytes"
+	"errors"
+	"io"
 	"net/http"
+
+	"github.com/srg-bnd/observator/internal/server/logger"
+	"go.uber.org/zap"
 )
 
 type ChecksumBehaviour interface {
 	Verify(string, string) error
+	Sum(string) (string, error)
 }
 
 type Checksum struct {
 	ChecksumService ChecksumBehaviour
 }
 
-const ErrReadBody = "couldn't read the body during verify"
+const hashKey = "HashSHA256"
+
+var ErrChecksumVerify = errors.New("bad checksum verify")
 
 func NewChecksum(checksumService ChecksumBehaviour) *Checksum {
 	return &Checksum{
@@ -23,22 +32,37 @@ func NewChecksum(checksumService ChecksumBehaviour) *Checksum {
 
 func (c *Checksum) WithVerify(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// body, err := io.ReadAll(r.Body)
-		// if err != nil {
-		// 	http.Error(w, err.Error(), http.StatusInternalServerError)
-		// 	logger.Log.Info(ErrReadBody, zap.Error(err))
-		// 	return
-		// }
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			logger.Log.Info(ErrChecksumVerify.Error(), zap.Error(err))
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		r.Body.Close()
 
-		// if r.Header.Get("HashSHA256") != "" && len(body) > 0 {
-		// 	if err := c.ChecksumService.Verify(r.Header.Get("HashSHA256"), string(body)); err != nil {
-		// 		logger.Log.Info("checksum verify:", zap.Error(err))
+		if expectedChecksum := r.Header.Get(hashKey); expectedChecksum != "" && len(body) > 0 {
+			if err := c.ChecksumService.Verify(expectedChecksum, string(body)); err != nil {
+				logger.Log.Info(ErrChecksumVerify.Error(), zap.Error(err))
+				http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+				return
+			}
+		}
 
-		// 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-		// 		return
-		// 	}
-		// }
-
+		r.Body = io.NopCloser(bytes.NewBuffer(body))
 		next.ServeHTTP(w, r)
+
+		responseBody, err := io.ReadAll(r.Response.Body)
+		if err != nil {
+			logger.Log.Info(ErrChecksumVerify.Error(), zap.Error(err))
+			return
+		}
+
+		checksum, err := c.ChecksumService.Sum(string(responseBody))
+		if err != nil {
+			logger.Log.Info(ErrChecksumVerify.Error(), zap.Error(err))
+			return
+		}
+
+		w.Header().Set(hashKey, checksum)
 	})
 }
